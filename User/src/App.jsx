9da091fb6bb357bc15a9e41f8484bed29c49df90
priya <sivapragasampriya01@gmail.com>
2025-08-06 23:44:44 +0530

    Add utility files and update package.json for module support

--- User/src/App.jsx
+++ User/src/App.jsx
@@ -1,16 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import React from 'react';
+import Page from "./pages/Home.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      {/* This is a comment */}
+      <Page />
     </>
   );
 }
