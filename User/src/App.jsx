85593cecd36c590dd8597a2b97f81a730c68e81e
priya <sivapragasampriya01@gmail.com>
2025-08-07 18:49:38 +0530

    Implement routing and footer in App component

--- User/src/App.jsx
+++ User/src/App.jsx
@@ -2,11 +2,22 @@ import Page from "./pages/Home.jsx";
 
 
 
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import React from 'react';
+import Footer from "./Student/Components/Footer";
+import {Routes, Route} from 'react-router-dom';
+
 function App() {
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Page />
+      <Routes>
+      <Route path="/" element={<Home />} />      </Routes>
+      <Footer />
     </>
   );
 }
