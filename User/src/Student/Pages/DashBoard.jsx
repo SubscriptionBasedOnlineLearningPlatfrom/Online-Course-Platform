5f398f0108122358669ce2716db4b566faa5fcb7
ParkkaviSivakaran72 <mathujasivakaran41@gmail.com>
2025-08-10 18:49:14 +0530

    dashboard page with completed course and enrolled courses

--- User/src/Student/Pages/DashBoard.jsx
+++ User/src/Student/Pages/DashBoard.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import EnrolledCourses from '../Components/DashBoard/EnrolledCourses'
-import CourseCard from '../Components/DashBoard/CourseCard'
 
 const DashBoard = () => {
   return (
     <div>
         <EnrolledCourses />
-        <CourseCard />
+        
     </div>
   )
 }
