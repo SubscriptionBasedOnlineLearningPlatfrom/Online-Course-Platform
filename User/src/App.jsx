24d0f682f0178b4b0a13c9658c26f00aadcf25b1
Vidurshika <165041842+Vidurshika@users.noreply.github.com>
2025-08-12 00:50:53 +0530

    changes

--- User/src/App.jsx
+++ User/src/App.jsx
@@ -1,18 +1,14 @@
 import Home from "./pages/Home.jsx";
-
-
-
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import React from 'react';
-
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar.jsx";
 import Courses from "./pages/Courses";
 import Footer from "./Student/Components/Footer";
-import {Routes, Route} from 'react-router-dom';
+
 
 
 function App() {
