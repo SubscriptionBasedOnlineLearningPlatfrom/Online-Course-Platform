0d853e23e021a476c93bb6570115260f69d36cb8
Vidurshika <165041842+Vidurshika@users.noreply.github.com>
2025-08-12 01:08:30 +0530

    Update main.jsx

--- User/src/main.jsx
+++ User/src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'; // ADD THIS
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 import {BrowserRouter} from 'react-router-dom'
