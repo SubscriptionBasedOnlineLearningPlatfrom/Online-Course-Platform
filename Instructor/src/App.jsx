faf25ac6915e0e06d2192aa82daed4bfed5cb15f
Vidurshika <165041842+Vidurshika@users.noreply.github.com>
2025-08-20 11:04:20 +0530

    set up routes

--- Instructor/src/App.jsx
+++ Instructor/src/App.jsx
@@ -4,19 +4,31 @@ import './App.css'
 import React from 'react'; 
 import LearnerFeedbackCarousel from './components/LearnerFeedbackCarousel';
 import AddCourse from './components/course/addCourse';
+import CurriculumPage from './pages/Curriculum';
+import Sidebar from './components/Sidebar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+      <div>
+        <Sidebar />
+        <div className="ml-0 md:ml-64 lg:ml-72 p-6">
+          <Routes>
+  {/*         <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/courses/add" element={<AddCourse />} />
+              <Route path="/courses" element={<CourseList />} />
+              <Route path="/students" element={<Students />} /> */}
+              <Route
+                path="/courses/:courseId/curriculum"
+                element={<CurriculumPage />}
+              />
+          </Routes>
+          {/*<LearnerFeedbackCarousel/>temporily added for testing - insert component to the correct position of the dashboard */}
+        </div>
+      </div>
 
-      <Routes>
-        <Route path="/add-course" element={<AddCourse />} />
-      </Routes>
 
-      <LearnerFeedbackCarousel/> {/* temporily added for testing - insert component to the correct position of the page */}
-    </>
   )
 }
 
