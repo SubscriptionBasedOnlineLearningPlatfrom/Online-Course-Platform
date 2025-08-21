806196bc03e40912ee0a862eac17304704c9fb07
ParkkaviSivakaran72 <mathujasivakaran41@gmail.com>
2025-08-21 17:18:25 +0530

    added quiz creation page

--- Instructor/src/App.jsx
+++ Instructor/src/App.jsx
@@ -6,6 +6,7 @@ import AddCourse from './components/course/addCourse'
 import Pricing from './Pages/Pricing';
 import ViewCreatedCourse from './Components/Course/ViewCreatedCourse';
 import EnrollmentOverview from './Components/Enrollments/EnrollmentOverview';
+import QuizCreation from './Components/Quizes/QuizCreation';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,6 +19,7 @@ function App() {
         <Route path="/ViewCreatedCourse" element={<ViewCreatedCourse />} />
         <Route path="/EnrollmentOverview" element={<EnrollmentOverview />} />
         <Route path="/EnrollmentOverview" element={<EnrollmentOverview />} />
+        <Route path="/QuizCreation" element={<QuizCreation />} />
         
         
       </Routes>
