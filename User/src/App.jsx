3d3f2b1291f43fa0afc9b9ceda6a468f9f84446f
ParkkaviSivakaran72 <mathujasivakaran41@gmail.com>
2025-08-28 21:20:53 +0530

    changed pricing page

--- User/src/App.jsx
+++ User/src/App.jsx
@@ -11,30 +11,38 @@ import DisplayCourse from "./Student/Pages/DisplayCourse";
 import DashBoard from "./Student/Pages/DashBoard";
 import CourseProgress from "./Student/Components/DashBoard/CourseProgress";
 import CertificatePage from "./Student/Pages/Certificate";
-import { QuizComponent } from '../../User/src/Student/Components/Quizes/Quiz';
+import { QuizComponent } from "../../User/src/Student/Components/Quizes/Quiz";
 import CourseContentPage from "./Student/Components/DashBoard/CourseContentPage";
-
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Navbar/>
-      <Toaster richColors position="top-center" /> {/* Add the Toaster component here */}
+      <Navbar />
+      <Toaster richColors position="top-center" />{" "}
+      {/* Add the Toaster component here */}
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
+        <Route path="/" element={<Home />}></Route>
         {/* <Route path="/courses" element={<DisplayCourse />}></Route> --displaycourse is to show a single course when a course from courses is clicked*/}
         <Route path="/dashboard" element={<DashBoard />} />
         {/* <Route path="/" element={<Home />} /> */}
-        <Route path="/courses" element={<Courses />} /> {/* this is a page to display all courses which are visible to public users who are not logged in */}
-
-        <Route path="/courses/:courseId/progress" element={<CourseProgress />} />  {/* student progress of the logged in student*/}
+        <Route path="/courses" element={<Courses />} />{" "}
+        {/* this is a page to display all courses which are visible to public users who are not logged in */}
+        <Route
+          path="/courses/:courseId/progress"
+          element={<CourseProgress />}
+        />{" "}
+        {/* student progress of the logged in student*/}
         <Route path="/certificate/:courseId" element={<CertificatePage />} />
-
         <Route path="/displayCourses" element={<DisplayCourse />} />
-        <Route path="/courses/:courseId/content" element={<CourseContentPage />} />  {/* student progress of the logged in student*/}
-        <Route path="/displayCourses" element={<DisplayCourse />} /> {/* this is a page to display all courses which are visible to public users who are not logged in */}
+        <Route
+          path="/courses/:courseId/content"
+          element={<CourseContentPage />}
+        />{" "}
+        {/* student progress of the logged in student*/}
+        <Route path="/displayCourses" element={<DisplayCourse />} />{" "}
+        {/* this is a page to display all courses which are visible to public users who are not logged in */}
         <Route path="/QuizComponent" element={<QuizComponent />} />
       </Routes>
       <Footer />
