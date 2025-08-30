f2feed98a499d2fbc748a403db8fd3361112fa5c
ParkkaviSivakaran72 <mathujasivakaran41@gmail.com>
2025-08-30 23:36:25 +0530

    Successfully added database with backend

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config';
 import cookieParser from "cookie-parser";
+import { supabase } from "./Database/SupabaseClient.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -17,4 +18,12 @@ app.get("/", (req, res) => {
 // Start the server
 app.listen(port, () => {
   console.log(`Server started on PORT: ${port}`);
+});
+
+
+// Supabase setup
+app.get("/users", async (req, res) => {
+  const { data, error } = await supabase.from("users").select("*");
+  if (error) return res.status(500).json({ error: error.message });
+  res.json(data);
 });
