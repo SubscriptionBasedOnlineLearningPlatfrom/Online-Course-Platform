8a833698fe74c98f3e1f0fe8aa90844ca665c780
ParkkaviSivakaran72 <mathujasivakaran41@gmail.com>
2025-09-05 19:11:49 +0530

    create, update, delte reply for comment in instructor backend

--- server/Controllers/Instructor/CommentsController.js
+++ server/Controllers/Instructor/CommentsController.js
@@ -1,52 +1,144 @@
 import { supabase } from "../../Database/SupabaseClient.js";
+import { z } from 'zod';
 
-
-export const viewStudentsComments = async (req,res) => {
+export const viewStudentsComments = async (req, res) => {
     try {
         // const instructor_id = req.instructorId;
-        const {instructor_id} = req.body;
-        
-        const {data : comments , commentError} =await  supabase
-                            .from('students_comments_for_instructor')
-                            .select('*')
-                            .eq('instructor_id', instructor_id);
-        
-        if(commentError){
-            return res.status().json({error : error.message})
-        }
-        
+        const { instructor_id } = req.body;
 
-        const {data : replies , repliesError } = await supabase
-                                                        .from('instructor_replies_for_comments')
-                                                        .select('*')
-                                                        // .eq('instructor_id', instructor_id)
-                                                        .order("updated_at", {ascending : false});
-                                                    
-        if(repliesError){
-            return res.status(500).json({error : error.message})
+        const { data: comments, commentError } = await supabase
+            .from('students_comments_for_instructor')
+            .select('*')
+            .eq('instructor_id', instructor_id);
+
+        if (commentError) {
+            return res.status().json({ error: error.message })
+        }
+
+
+        const { data: replies, repliesError } = await supabase
+            .from('instructor_replies_for_comments')
+            .select('*')
+            // .eq('instructor_id', instructor_id)
+            .order("updated_at", { ascending: false });
+
+        if (repliesError) {
+            return res.status(500).json({ error: error.message })
         }
         // console.log(replies);
         const repliesByComment = (replies || []).reduce((acc, reply) => {
             (acc[reply.comment_id] ||= []).push(reply);
             return acc;
-        },{})
+        }, {})
 
         // console.log(repliesByComment)
 
-        const withReplies = (comments || []).reduce((acc,comment) => {
-            acc[comment.comment_id]  = {
+        const withReplies = (comments || []).reduce((acc, comment) => {
+            acc[comment.comment_id] = {
                 ...comment,
-                replies : repliesByComment[comment.comment_id] || []
+                replies: repliesByComment[comment.comment_id] || []
             }
-            return acc[comment.comment_id];
-            
-        })
-        console.log(withReplies);
+            return acc;
+
+        }, {})
+        console.log(JSON.stringify(withReplies, null, 2));
         return res.status(200).json(withReplies);
 
 
     } catch (error) {
         console.log(error)
-        return res.status(500).json({error: "Internal Server Error : ", details: error.message})
+        return res.status(500).json({ error: "Internal Server Error : ", details: error.message })
     }
+}
+
+
+// create a reply for comments
+const createReplySchema = z.object({
+    comment_id: z.uuid(),
+    instructor_id: z.uuid(),
+    reply_text: z.string().min(1).max(5000)
+})
+
+export const createReplyForComment = async (req, res) => {
+    try {
+        const parsed = createReplySchema.safeParse({
+            comment_id: req.params.commentId,
+            instructor_id: req.body.instructor_id,
+            reply_text: req.body.reply_text
+        })
+
+        if (!parsed.success) {
+            return res.status(400).json({ error: parsed.error });
+        }
+
+        const { data, error } = await supabase
+            .from("instructor_replies")
+            .insert(parsed.data)
+            .select('*')
+            .single();
+
+        if (error) {
+            return res.status(500).json({ error: error.message });
+        }
+        console.log(data);
+        return res.json(data);
+
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({ error: "Internal server error", details: error.message });
+    }
+}
+
+// update a reply
+
+const updateReplySchema = z.object({
+    reply_text: z.string().min(1).max(5000)
+})
+
+export const updateReplyForComment = async (req, res) => {
+    try {
+        const parsed = updateReplySchema.safeParse({
+            reply_text: req.body.reply_text
+        })
+
+        if (!parsed.success) {
+            return res.status(400).json({ error: parsed.error });
+        }
+
+        const { data, error } = await supabase
+            .from("instructor_replies")
+            .update({ reply_text: parsed.data.reply_text, updated_at: new Date().toISOString() })
+            .eq('reply_id', req.params.replyId)
+            .select('*')
+            .single();
+
+        if (error) {
+            return res.status(400).json({ error: error.message });
+        }
+
+        return res.json(data);
+
+    } catch (error) {
+        return res.status(500).json({ error: "Internal server error", details: error.message });
+    }
+}
+
+// delete a reply
+export const deleteReply = async (req, res) => {
+
+    try {
+        const { error } = await supabase
+            .from("instructor_replies")
+            .delete()
+            .eq("reply_id", req.params.replyId);
+
+        if (error) {
+            return res.status(400).json({ error: error.message });
+        }
+
+        return res.status(200).send()
+    } catch (error) {
+        return res.status(500).json({ error: "Internal server error", details: error.message });
+    }
+
 }
--- server/Routers/Instructor/CommentsRouter.js
+++ server/Routers/Instructor/CommentsRouter.js
@@ -1,8 +1,12 @@
 import express from 'express'
-import { viewStudentsComments } from '../../Controllers/Instructor/CommentsController.js';
+import { createReplyForComment, deleteReply, updateReplyForComment, viewStudentsComments } from '../../Controllers/Instructor/CommentsController.js';
 
 const commentRouter = express.Router();
 
 commentRouter.get("/comments", viewStudentsComments);
+commentRouter.post("/:commentId/replies", createReplyForComment);
+commentRouter.put("/update-reply/:replyId", updateReplyForComment);
+commentRouter.delete("/delete-reply/:replyId", deleteReply);
+
 
 export default commentRouter;
