3a1ec16bf7b8caf76b1f0e95c19e54a67d4d3d2f
Vidurshika <165041842+Vidurshika@users.noreply.github.com>
2025-09-16 09:18:42 +0530

    Update CourseRouter.js

--- server/Routers/Student/CourseRouter.js
+++ server/Routers/Student/CourseRouter.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import { courseDetails, enrollment } from '../../Controllers/Student/CourseController.js';
+import { fetchCourses } from "../../Controllers/Student/getAllCoursesController.js";
 
 const courseRouter = express.Router();
 
 courseRouter.get('/:courseId', courseDetails);
 courseRouter.post('/enrollment', enrollment);
+
+courseRouter.get("/", fetchCourses);
 
 export default courseRouter;
