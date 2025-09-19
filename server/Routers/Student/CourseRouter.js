f4a4c405cff182d47fceac3874d4a25c498a2793
ParkkaviSivakaran72 <mathujasivakaran41@gmail.com>
2025-09-19 09:21:13 +0530

    related courses were added

--- server/Routers/Student/CourseRouter.js
+++ server/Routers/Student/CourseRouter.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import { courseDetails, enrollment } from '../../Controllers/Student/CourseController.js';
+import { courseDetails, enrollment, fetchRelatedCourses } from '../../Controllers/Student/CourseController.js';
 import { fetchCourses } from "../../Controllers/Student/getAllCoursesController.js";
 
 const courseRouter = express.Router();
 
 courseRouter.get('/:courseId', courseDetails);
 courseRouter.post('/enrollment', enrollment);
-
+courseRouter.get("/related-courses/:category",fetchRelatedCourses);
 courseRouter.get("/", fetchCourses);
 
 export default courseRouter;
