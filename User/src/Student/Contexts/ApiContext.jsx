c77ed95b753d272e7d30bb4dbd5e13fd6d94c92f
ParkkaviSivakaran72 <mathujasivakaran41@gmail.com>
2025-09-19 09:40:21 +0530

    related courses were added

--- User/src/Student/Contexts/ApiContext.jsx
+++ User/src/Student/Contexts/ApiContext.jsx
@@ -1,73 +1,15 @@
-import React, { createContext, useContext } from "react";
-import axios from "axios";
-
-// =======================
-// PRIVATE API
-// =======================
-const api = axios.create({
-  baseURL: "http://localhost:4000",
-});
-
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) config.headers.Authorization = `Bearer ${token}`;
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    const message = error.response?.data?.message || "Something went wrong";
-    return Promise.reject(new Error(message));
-  }
-);
-
-// =======================
-// PUBLIC API
-// =======================
+import React,{ createContext, useState } from "react";
 
 
-const publicApi = axios.create({
-  baseURL: "http://localhost:4000",
-});
+export const APIContext = createContext(); 
 
-publicApi.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    const message = error.response?.data?.message || "Something went wrong";
-    return Promise.reject(new Error(message));
-  }
-);
+export const APIProvider = ({ children }) => {
+    const BackendAPI = "http://localhost:4000/student";
 
-// =======================
-// API FUNCTIONS
-// =======================
+    return (
+        <APIContext.Provider value={{ BackendAPI }}>
+            {children}
+        </APIContext.Provider>
+    );
+}
 
-const getAllCourses = async () => {
-  const response = await publicApi.get("/student/courses");
-  return response.data;
-};
-
-
-
-
-const ApiContext = createContext({});
-
-export const ApiProvider = ({ children }) => {
-  return (
-    <ApiContext.Provider
-      value={{
-        api,
-        publicApi,
-        getAllCourses, // as a function to children
-      }}
-    >
-      {children}
-    </ApiContext.Provider>
-  );
-};
-
-export const useApi = () => useContext(ApiContext);
