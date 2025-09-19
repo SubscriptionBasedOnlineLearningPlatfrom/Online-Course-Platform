12efe9fba53b420d440278a638ea0870830770a7
Vidurshika <165041842+Vidurshika@users.noreply.github.com>
2025-09-19 20:09:15 +0530

    update routes

--- server/Routers/Instructor/chapterRoutes.js
+++ server/Routers/Instructor/chapterRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import { addChapter, deleteChapter } from '../../Controllers/Instructor/chapterController.js';
-import { Instructor } from '../../Middleware/authInstructor.js';
+import { auth } from '../../Middleware/authMiddleware.js';
 
 const router = express.Router();
 
 // Add a chap
-router.post('/:moduleId', Instructor, addChapter);
+router.post('/:moduleId', auth, addChapter);
 
 //delete a chap(lesson==chapter)
-router.delete('/:lessonId', Instructor, deleteChapter);
+router.delete('/:lessonId', auth, deleteChapter);
 
 export default router;
--- server/Routers/Instructor/moduleRoutes.js
+++ server/Routers/Instructor/moduleRoutes.js
@@ -1,17 +1,17 @@
 import express from 'express';
 import { addModule, listModules, deleteModule } from '../../Controllers/Instructor/moduleController.js';
-import { Instructor } from '../../Middleware/authInstructor.js';
+import { auth } from '../../Middleware/authMiddleware.js';
 
 const router = express.Router();
 
 // Add a new module to a course
-router.post('/:courseId', Instructor, addModule);
+router.post('/:courseId', auth, addModule);
 
 // Get all modules for a course
-router.get('/:courseId', Instructor, listModules);
+router.get('/:courseId', auth, listModules);
 
 // delete module
-router.delete('/:moduleId', Instructor, deleteModule);
+router.delete('/:moduleId', auth, deleteModule);
 
 
 export default router;
